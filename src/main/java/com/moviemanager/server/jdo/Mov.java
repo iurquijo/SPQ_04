46b710ae38e229d0824d2af1a2a7774a311784ed
Serhat Sahin <serhatr7@gmail.com>
2016-05-26 13:08:22 +0200

    new modifications

---
+++ src/main/java/com/moviemanager/server/jdo/Mov.java
@@ -0,0 +1,109 @@
+package com.moviemanager.server.jdo;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.jdo.annotations.Join;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+
+@PersistenceCapable
+
+public class Mov{
+
+	/**
+	 * User implements Serializable to be transferred to the RMI client
+	 */
+
+	@PrimaryKey
+	String nameM = null;
+	String rate = null;
+	String numRates = null;
+	String description = null;
+	Drector director=null;
+	
+	/*@Persistent(mappedBy = "user", dependentElement = "true")
+	@Join*/
+	List<Comment> commentsM = new ArrayList<Comment>();
+	
+	/*@Persistent(mappedBy = "name", dependentElement = "true")
+	@Join*/
+	List<Actor> actorsM = new ArrayList<Actor>();
+	public Mov(){
+		this.nameM="";
+		this.rate="";
+		this.numRates="";
+		this.description="";
+		this.director=null;
+	
+	}
+	public Mov(String nameM, String rate, String numRates, String description, Drector director, List<Comment> commentsM, List<Actor> actorsM) {
+		super();
+		this.nameM = nameM;
+		this.rate = rate;
+		this.numRates = numRates;
+		this.description=description;
+		this.director=director;
+		this.commentsM = commentsM;
+		this.actorsM = actorsM;
+	}
+	private static final long serialVersionUID = 1L;
+	
+	public String getNameM() {
+		return nameM;
+	}
+
+	public void setNameM(String nameM) {
+		this.nameM = nameM;
+	}
+
+	public String getRate() {
+		return rate;
+	}
+
+	public void setRate(String rate) {
+		this.rate = rate;
+	}
+
+	public String getNumRates() {
+		return numRates;
+	}
+
+	public void setNumRates(String numRates) {
+		this.numRates = numRates;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Drector getDirector() {
+		return director;
+	}
+
+	public void setDirector(Drector director) {
+		this.director= director;
+	}
+
+	public List<Comment> getCommentsM() {
+		return commentsM;
+	}
+
+	public void setCommentsM(List<Comment> commentsM) {
+		this.commentsM = commentsM;
+	}
+
+	public List<Actor> getActorsM() {
+		return actorsM;
+	}
+
+	public void setActorsM(List<Actor> actorsM) {
+		this.actorsM = actorsM;
+	}
+}
---
+++ src/main/java/com/moviemanager/server/jdo/PlayLst.java
@@ -0,0 +1,81 @@
+package com.moviemanager.server.jdo;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.jdo.annotations.Join;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+
+
+import java.util.*;
+
+/**
+ * Created by sesion on 03/05/2016.
+ */
+@PersistenceCapable
+public class PlayLst implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+    private String name="";
+    private User user;
+    private ArrayList<Mov> playlistMovie = new ArrayList<Mov>();
+
+    
+    public PlayLst() {
+        name = "Untitled";
+        playlistMovie = new ArrayList<Mov>();
+    }
+
+    public PlayLst(String name) {
+        this.name = name;
+        playlistMovie = new ArrayList<Mov>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Mov> getplaylist() {
+        return playlistMovie;
+    }
+
+    public void setplaylist(ArrayList<Mov> playlist) {
+        this.playlistMovie = playlist;
+    }
+
+
+    public void introduceFilm(Mov film, int position){
+    	playlistMovie.add(position, film);
+    }
+
+    public void introduceFilm(Mov film){
+    	playlistMovie.add(film);
+    }
+
+    public void removeFilm(int pos){
+    	playlistMovie.remove(pos);
+
+    }
+
+    public void replaceFilm(int pos, Mov film){
+    	playlistMovie.remove(pos);
+    	playlistMovie.add(pos, film);
+    }
+
+   
+}
+
+    	
+    	
+
+
+
+
