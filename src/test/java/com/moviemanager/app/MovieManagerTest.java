913294f1ac5009a2af4b1d01e2a275d9a72c9f8c
sergior <sergiers@gmail.com>
2016-06-09 12:07:08 +0200

    more tests

--- src/test/java/com/moviemanager/app/MovieManagerTest.java
+++ src/test/java/com/moviemanager/app/MovieManagerTest.java
@@ -1,22 +1,42 @@
-package es.deusto.server;
+package com.moviemanager.app;
 
 
 import org.junit.*;
 
 import com.moviemanager.server.jdo.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import static org.junit.Assert.assertTrue;
 
 
 public class MovieManagerTest {
 
-    Mov agora;
+    //======MOVIE=====
+    private Mov agora;
+    //==COMMENT=====
+    Comment com;
+    String text;
+    User user;
 
+    //====USER====
+    String mail;
+    String nick;
+    String password;
+    List<Comment> commentsUser;
+    List<PlayLst> playListsUser;
 
     @Before
     public void setUp() {
-       agora = new Mov();
+        agora = new Mov();
+        com = new Comment();
+        String text = "";
+        String mail = "gmail@gmail.com";
+        String nick = "Nickname";
+        String password = "Password";
+        List<Comment> commentsUser = null;
+        List<PlayLst> playListsUser = null;
+        User user = new User( mail,  nick,  password,  commentsUser, playListsUser);
     }
 
     @Test
@@ -36,9 +56,44 @@ public class MovieManagerTest {
             //for example few values
             test = true;
         }
-
         assertTrue(test);
     }
 
+    @Test
+    public void testcomment(){
+        com = new Comment(text, user, agora);
+        assertTrue(agora.addComment(com));
+    }
+
+    @Test
+    public void testSearchMoviesByName(){
+        boolean test = false;
+        String nameOne = "Agora";
+        String nameTwo = "The Godfather";
+
+
+        //create a playlist and add Agora to it
+        Mov movie = new Mov();
+        movie.setNameM(nameOne);
+        ArrayList<Mov> list = new ArrayList<>();
+
+        list.add(0, movie);
+        PlayLst pl = new PlayLst(list);
+
+
+        //pl.setplaylist(list);
+
+        //Search the film The Godfather in the playlist
+        Mov searchedMovie = null;
+        searchedMovie= pl.searchMovieInPlayListByName(nameTwo);
+
+        //The movie TheGodfather isn't in the playlist, so the asert is false
+        if((searchedMovie != null)&&(searchedMovie.getNameM()==nameTwo)){
+            test = true;
+            assertTrue(test);
+        }else{
+            assertTrue(test);
+        }
+    }
 
 }
